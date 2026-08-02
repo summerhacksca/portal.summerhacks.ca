bf875d1591d759bbc1df68cbabf7ab137b845b7c
Aaryan Patel <86172867+patel-aaryan@users.noreply.github.com>
2026-08-02 12:32:07 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  allowedDevOrigins: ["pansophical-unmeaningfully-daysi.ngrok-free.dev"],
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
